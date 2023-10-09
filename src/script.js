c38badf0a50d9f6ef1a92f43e4eaeb457e9000fe
Konstantina Boua <85695279+KonsBo@users.noreply.github.com>
2023-10-09 14:17:16 +0300

    create a spin parameter

--- src/script.js
+++ src/script.js
@@ -24,6 +24,7 @@ parameters.count = 10000;
 parameters.size = 0.01;
 parameters.radius = 5;
 parameters.branches = 3;
+parameters.spin = 1;
 
 let geometry = null;
 let material = null;
@@ -49,12 +50,13 @@ const generateGalaxy = () => {
     const i3 = i * 3;
 
     const radius = Math.random() * parameters.radius;
+    const spinAngle = radius * parameters.spin;
     const branchAngle =
       ((i % parameters.branches) / parameters.branches) * Math.PI * 2;
 
-    positions[i3 + 0] = Math.cos(branchAngle) * radius;
+    positions[i3] = Math.cos(branchAngle + spinAngle) * radius;
     positions[i3 + 1] = 0;
-    positions[i3 + 2] = Math.sin(branchAngle) * radius;
+    positions[i3 + 2] = Math.sin(branchAngle + spinAngle) * radius;
   }
   geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3));
 
@@ -100,6 +102,12 @@ gui
   .min(2)
   .max(20)
   .step(1)
+  .onFinishChange(generateGalaxy);
+gui
+  .add(parameters, "spin")
+  .min(-5)
+  .max(5)
+  .step(0.001)
   .onFinishChange(generateGalaxy);
 
 /**
